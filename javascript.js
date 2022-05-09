b4f412b6362eae8e14906eb7f4c1abdc4dfec3c6
jacksoze <kamonyojack@gmail.com>
2022-05-09 17:50:16 +0300

    Delete everything and start again

--- javascript.js
+++ javascript.js
@@ -5,34 +5,9 @@ var choices = [
     'scissors'
 ]
 
-let computerPlay = choices[Math.floor(Math.random()*choices.length)];
-console.log(`the computer played ${computerPlay}`);
-
-let computerSelection = computerPlay
-
-let playerSelection = prompt('input',).toLowerCase()
-console.log(`the player played ${playerSelection}`)
-    
-function playRound (playerSelection,computerSelection) {
-    if ((playerSelection=='rock')&&(computerSelection=='rock')) {
-        console.log('the game was a tie')
-    } else if ((playerSelection=='rock')&&(computerSelection=='paper')) {
-        console.log('you lose, paper beats rock')
-    } else if ((playerSelection=='rock')&&(computerSelection=='scissors')) {
-        console.log('you win, rock beats scissors')
-    } else if ((playerSelection=='paper')&&(computerSelection=='rock')) {
-        console.log('you win, paper beats rock')
-    } else if ((playerSelection=='paper')&&(computerSelection=='paper')) {
-        console.log('the game was a tie')
-    } else if ((playerSelection=='paper')&&(computerSelection=='scissors')) {
-        console.log('you lose, scissors beats paper')
-    } else if ((playerSelection=='scissors')&&(computerSelection=='rock')) {
-        console.log('you lose, rock beats scissors')
-    } else if ((playerSelection=='scissors')&&(computerSelection=='paper')) {
-        console.log('you win, scissors beats paper')
-    } else if ((playerSelection=='scissors')&&(computerSelection=='scissors')) {
-        console.log('the game was a tie')
-    } 
+function  computerPlay() {
+    return (choices[Math.floor(Math.random()*choices.length)])
 }
 
-playRound(playerSelection,computerSelection)
+console.log(computerPlay())
+

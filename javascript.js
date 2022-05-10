e9a2fc74e87991fbf84dda8163b77c1d7751c531
jacksoze <kamonyojack@gmail.com>
2022-05-10 20:14:16 +0300

    Create the function game
    the function game() created. takes gameRound() as a callback

--- javascript.js
+++ javascript.js
@@ -6,38 +6,68 @@ var choices = [
 ]
 
 function  computerPlay() {
-    return (choices[Math.floor(Math.random()*choices.length)])
+    let play = (choices[Math.floor(Math.random()*choices.length)]); 
+    return play
 }
-
-let computerSelection = computerPlay()
-console.log(`the computer played ${computerSelection}`)
 
 let playerSelection = 'rock'
 console.log(`the player played ${playerSelection}`)
 
 function gameRound(playerSelection,computerSelection) {
     if ((playerSelection=='rock')&&(computerSelection=='rock')) {
-        console.log('tie')
+        return ('tie')
     } else if ((playerSelection=='rock')&&(computerSelection=='paper')) {
-        console.log('computer wins')
+        return ('computer wins')
     } else if ((playerSelection=='rock')&&(computerSelection=='scissors')) {
-        console.log('player wins')
+        return ('player wins')
     } else if ((playerSelection=='paper')&&(computerSelection=='rock')) {
-        console.log('player wins')
+        return ('player wins')
     } else if ((playerSelection=='paper')&&(computerSelection=='paper')) {
-        console.log('tie')
+        return ('tie')
     } else if ((playerSelection=='paper')&&(computerSelection=='scissors')) {
-        console.log('computer wins')
+        return ('computer wins')
     } else if ((playerSelection=='scissors')&&(computerSelection=='rock')) {
-        console.log('computer wins')
+        return ('computer wins')
     } else if ((playerSelection=='scissors')&&(computerSelection=='paper')) {
-        console.log('player wins')
+        return ('player wins')
     } else if  ((playerSelection=='scissors')&&(computerSelection=='scissors')) {
-        console.log('tie')
+        return ('tie')
     } 
 }
 
-gameRound(playerSelection,computerSelection)
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (i=0; i<5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = prompt('whats your input', ).toLowerCase()
+        let result = (gameRound(playerSelection,computerSelection));
+        if (result=='player wins'){
+            console.log('player')
+            playerWins=playerWins+1;
+        } else if (result=='computer wins') {
+            console.log ('computer')
+            computerWins=computerWins+1;
+        } else {
+            console.log('tie')
+        }
+        
+
+    }
+    console.log (playerWins)
+    console.log (computerWins)
+}
+
+console.log(game())
+
+function winner(playerWins,computerWins){
+    if (playerWins>computerWins) {
+        console.log('the player wins')
+    } else {
+        console.log('the computer wins')
+    }
+}
 
 
 

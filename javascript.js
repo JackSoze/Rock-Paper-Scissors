5145ccd46f3cef30987c51d64a65b5803acb031f
jacksoze <kamonyojack@gmail.com>
2022-05-10 19:11:30 +0300

    Create the playRound function

--- javascript.js
+++ javascript.js
@@ -9,5 +9,37 @@ function  computerPlay() {
     return (choices[Math.floor(Math.random()*choices.length)])
 }
 
-console.log(computerPlay())
+let computerSelection = computerPlay()
+console.log(`the computer played ${computerSelection}`)
+
+let playerSelection = 'rock'
+console.log(`the player played ${playerSelection}`)
+
+function gameRound(playerSelection,computerSelection) {
+    if ((playerSelection=='rock')&&(computerSelection=='rock')) {
+        console.log('tie')
+    } else if ((playerSelection=='rock')&&(computerSelection=='paper')) {
+        console.log('computer wins')
+    } else if ((playerSelection=='rock')&&(computerSelection=='scissors')) {
+        console.log('player wins')
+    } else if ((playerSelection=='paper')&&(computerSelection=='rock')) {
+        console.log('player wins')
+    } else if ((playerSelection=='paper')&&(computerSelection=='paper')) {
+        console.log('tie')
+    } else if ((playerSelection=='paper')&&(computerSelection=='scissors')) {
+        console.log('computer wins')
+    } else if ((playerSelection=='scissors')&&(computerSelection=='rock')) {
+        console.log('computer wins')
+    } else if ((playerSelection=='scissors')&&(computerSelection=='paper')) {
+        console.log('player wins')
+    } else if  ((playerSelection=='scissors')&&(computerSelection=='scissors')) {
+        console.log('tie')
+    } 
+}
+
+gameRound(playerSelection,computerSelection)
+
+
+
+
 

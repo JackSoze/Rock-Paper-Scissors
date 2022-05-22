ff2275449c2e80ba197edf112981558f16424a24
jacksoze <kamonyojack@gmail.com>
2022-05-22 16:27:38 +0300

    Fix bug
    the computer was always displayed as winner

--- javascript.js
+++ javascript.js
@@ -14,13 +14,14 @@ function computerPlay() {
 let computerSelection = computerPlay();
 let playerwins = 0;
 let computerwins = 0;
+let gameWinner = ''
 
 
 function gameRound(playerSelection, computerSelection) {
     if ((playerSelection == 'rock') && (computerSelection == 'rock')) {
         return ('tie')
     } else if ((playerSelection == 'rock') && (computerSelection == 'paper')) {
-        return ('player wins')
+        return ('computer wins')
     } else if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {
         return ('player wins')
     } else if ((playerSelection == 'paper') && (computerSelection == 'rock')) {
@@ -44,34 +45,32 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button=> 
     button.addEventListener('click', function(e) {
     if ((playerwins==5)||(computerwins==5)) {
-        winner.textContent = `the winner is : ${playWinner}`
+        winner.textContent = `the winner is : ${gameWinner}`
         return;  
     }
     playerSelection = e.target.id;
-    console.log(playerSelection);
     computerSelection = computerPlay();
-    console.log(computerSelection);
     if (gameRound(playerSelection,computerSelection)=='player wins') {
         playerwins++
     } else if (gameRound(playerSelection,computerSelection)=='computer wins') {
         computerwins++
-    } else alert ('round was a tie')
+    } else { 
+        alert('the game was a tie')
+    }
+    
     playerWins.textContent =`player wins: ${playerwins}`;
     computerWins.textContent = `computer wins: ${computerwins}`;
+
+    if (playerwins>computerwins) {
+        gameWinner = 'player'
+    }   else {
+        gameWinner = 'computer'
+    }
     
 }) )
 
-let playWinner = ''
-
-if (playerwins>computerwins) {
-    playWinner = 'player'
-}   else {
-    playWinner = 'computer'
-}
 
 
-console.log(playerwins);
-console.log(computerwins);
 
 const header = document.querySelector('#header')
 const results = document.querySelector('#results');
